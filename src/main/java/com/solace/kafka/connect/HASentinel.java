b22307e30f4b20e15d3d9961838c6ded0d559899
Mic Hussey <Michael.Hussey@solace.com>
2018-03-13 16:54:04 +0100

    Simplified greatly by using Ken Overton's solft

--- src/main/java/com/solace/kafka/connect/HASentinel.java
+++ src/main/java/com/solace/kafka/connect/HASentinel.java
@@ -1,168 +1,62 @@
 package com.solace.kafka.connect;
 
-import org.apache.kafka.connect.errors.ConnectException;
+import java.util.concurrent.atomic.AtomicBoolean;
+
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
+import com.solace.solft.FTEventListener;
+import com.solace.solft.FTMgr;
 import com.solacesystems.jcsmp.BytesXMLMessage;
-import com.solacesystems.jcsmp.ConsumerFlowProperties;
-import com.solacesystems.jcsmp.DeliveryMode;
-import com.solacesystems.jcsmp.EndpointProperties;
-import com.solacesystems.jcsmp.FlowEvent;
-import com.solacesystems.jcsmp.FlowEventArgs;
-import com.solacesystems.jcsmp.FlowEventHandler;
-import com.solacesystems.jcsmp.FlowReceiver;
 import com.solacesystems.jcsmp.JCSMPException;
-import com.solacesystems.jcsmp.JCSMPFactory;
-import com.solacesystems.jcsmp.JCSMPProperties;
 import com.solacesystems.jcsmp.JCSMPSession;
-import com.solacesystems.jcsmp.JCSMPStreamingPublishEventHandler;
-import com.solacesystems.jcsmp.Queue;
-import com.solacesystems.jcsmp.XMLMessageListener;
-import com.solacesystems.jcsmp.XMLMessageProducer;
 
-public class HASentinel implements XMLMessageListener, JCSMPStreamingPublishEventHandler, FlowEventHandler {
+public class HASentinel implements FTEventListener {
 	
 	private static final Logger log = LoggerFactory.getLogger(HASentinel.class);
 	
 	protected JCSMPSession session;
 	
 	protected String haQueueName;
-	protected Queue haQueue;
 	
-	protected boolean isActiveMember = false;
+	private boolean isAM;
 
+	protected String id;
+	
 	public boolean isActiveMember() {
-		return isActiveMember;
+		return isAM;
 	}
-	private FlowReceiver consumer;
-
-	private boolean isStopped = true;
 	
-	public HASentinel(JCSMPSession _session, String queueName){
+	private FTMgr ftmanager;
+		
+	public HASentinel(String _id, JCSMPSession _session, String queueName) throws JCSMPException
+	{
+		this.id = _id;
 		session = _session;
-		haQueueName = queueName;
-	}
-	
-	public void connect() throws JCSMPException {
-		if (haQueueName == null) {
-			throw new ConnectException("Cannot create HA Sentinel as no HA Queue Name defined.");
-		}
-		haQueue = connectHAQ();
-		log.info("Provisioned HA Queue named {} on session {}", haQueueName, session.getSessionName());
-	}
-
-	protected Queue connectHAQ() throws JCSMPException {
-        log.debug("Attempting to provision the queue '{}' on the appliance", haQueueName);
-        
-        final EndpointProperties endpointProps = new EndpointProperties();
-        
-        // set queue permissions to "consume" and access-type to "exclusive"
-        endpointProps.setPermission(EndpointProperties.PERMISSION_CONSUME);
-        endpointProps.setAccessType(EndpointProperties.ACCESSTYPE_EXCLUSIVE);
-        
-        // We want this Q to be a Last Message Queue.
-        endpointProps.setQuota(0);
-        
-        // create the queue object locally
-        Queue _queue = JCSMPFactory.onlyInstance().createQueue(haQueueName);
-        // Actually provision it, and do not fail if it already exists
-        session.provision(_queue, endpointProps, JCSMPSession.FLAG_IGNORE_ALREADY_EXISTS);
-        
-        return _queue;
-	}
-	
-	protected void startListening() throws JCSMPException  {
-        // Create a Flow be able to bind to and consume messages from the Queue.
-        final ConsumerFlowProperties flow_prop = new ConsumerFlowProperties();
-        flow_prop.setEndpoint(haQueue);
-        flow_prop.setAckMode(JCSMPProperties.SUPPORTED_MESSAGE_ACK_CLIENT);
-        // magic flag so that we're notified when we become active
-        flow_prop.setActiveFlowIndication(true);
-
-        EndpointProperties endpoint_props = new EndpointProperties();
-        endpoint_props.setAccessType(EndpointProperties.ACCESSTYPE_EXCLUSIVE);
-
-        consumer = session.createFlow(this, flow_prop, endpoint_props, this);
-        
-        consumer.start();  
-	}
-	
-	/**
-	protected void sendHeartbeat() throws JCSMPException {
-		XMLMessageProducer prod = session.getMessageProducer(this);
-		BytesXMLMessage hbMessage = JCSMPFactory.onlyInstance().createMessage(BytesXMLMessage.class);
-		hbMessage.setDeliveryMode(DeliveryMode.PERSISTENT);
-		prod.send(hbMessage, haQueue);
-		
-		log.info("Sent HA heartbeat");
-	}
-	*/
-	
-
-	@Override
-	public void onException(JCSMPException arg0) {
-		log.warn(arg0.getMessage());
-	}
-
-	@Override
-	public void onReceive(BytesXMLMessage arg0) {
-		
-		arg0.ackMessage();
-		isActiveMember = true;
+		this.haQueueName = queueName;
+		//ftmanager = new FTMgr(session);
 		
 	}
-
-	@Override
-	public void handleError(String arg0, JCSMPException arg1, long arg2) {
-		log.warn("Error received when sending sentinel heartbeat {} {} {}",arg0, arg2, arg1.getMessage());
-		
-	}
-
-	@Override
-	public void responseReceived(String messageID) {
-		log.info("Successfully published Sentinel Heartbeat with message id {}", messageID);
-	}
-
+	
 	public void start()  throws JCSMPException {
-		//sendHeartbeat();
-		if (consumer == null)
-			startListening();
-		else
-			consumer.start();
-		isStopped = false;
+		this.ftmanager = new FTMgr(session);
+		this.ftmanager.start(haQueueName, this);
 	}
 	
 	public void stop()  throws JCSMPException {
-		consumer.stop();
-		consumer.close();
-		consumer = null;
-		
-		// Send a HB so that the next member of the group can become active
-		//sendHeartbeat();
-		isActiveMember = false;
-		isStopped = true;
+		this.ftmanager.stop();
 	}
-	/**
-	public void close()  throws JCSMPException {
-		if (!isStopped )
-			stop();
-		consumer.close();
-		consumer = null;
-	}/
 
-	/**
-	 * From FlowEventHandler
-	 */
 	@Override
-	public void handleEvent(Object source, FlowEventArgs event) {
-		log.info("Received event on Flow:"+source+" "+event);
-		if (event.getEvent() == FlowEvent.FLOW_ACTIVE) {
-			isActiveMember = true;
-		}
-		if (event.getEvent() == FlowEvent.FLOW_INACTIVE) {
-			isActiveMember = false;
-		}
+	public void onActive(BytesXMLMessage msg) {
+		log.info("HASentinel:{} became active", id);
+		isAM = true;
+	}
+
+	@Override
+	public void onBackup() {
+		log.info("HASentinel:{} became inactive", id);
+		isAM = false;
 	}
 
 }
